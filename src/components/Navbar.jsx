9359b18b60629f88fd5a0a54b6b8fa6df3e038a6
renayoo <rena.yoo@hotmail.com>
2025-06-08 17:01:28 +0200

    Routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-navbar shadow-md py-5 px-6 flex justify-between items-center">
-      <a href="/">
+      <Link to="/">
         <img
           src="/faviconLogo/faviconrecy.jpg"
           alt="Renayoo logo"
           className="h-15 w-auto"
         />
-      </a>
+      </Link>
       <ul className="font-savate text-black text-2xl font-bold flex gap-4 ">
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link
+            to="/projects"
+            className="transition-all duration-300 ease-out hover:scale-110 hover:font-semibold hover:text-white"
+          >
+            Projects
+          </Link>
         </li>
       </ul>
     </nav>
